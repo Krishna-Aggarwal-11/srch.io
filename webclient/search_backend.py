7b55c8c4e9eb80b413a356f93cc71bfc8ef0bde6
himanshu <110653014+nerdlab53@users.noreply.github.com>
2024-02-15 21:19:54 +0530

    Update search_backend.py

---
+++ webclient/search_backend.py
@@ -0,0 +1,98 @@
+import concurrent.futures
+import glob
+import json
+import os
+import re
+import threading
+import requests
+import traceback
+from typing import Annotated, List, Generator, Optional
+
+from fastapi import HTTPException
+from fastapi.responses import HTMLResponse, StreamingResponse, RedirectResponse
+import httpx
+from loguru import logger
+
+import leptonai
+from leptonai import Client
+from leptonai.kv import KV
+from leptonai.photon import Photon, StaticFiles
+from leptonai.photon.types import to_bool
+from leptonai.api.workspace import WorkspaceInfoLocalRecord
+from leptonai.util import tool
+
+# only using serper/searchapi for this
+SERPER_SEARCH_ENDPOINT = "https://google.serper.dev/search"
+SEARCHAPI_SEARCH_ENDPOINT = "https://www.searchapi.io/api/v1/search"
+
+
+# reference count 
+REFERENCES = 4
+
+# return error after exceeding the limit below
+DEFAULT_SEARCH_ENGINE_TIMEOUT = 5
+
+
+# def query 
+def_query = "Which character on the show 'The Big Bang Theory' idolizes Spock the most?"
+
+# rag query
+_rag_query_text = """
+You are a large language AI assistant. You are given a user question, and please write clean, concise and accurate answer to the question. You will be given a set of related contexts to the question, each starting with a reference number like [[citation:x]], where x is a number. Please use the context and cite the context at the end of each sentence if applicable.
+
+Your answer must be correct, accurate and written by an expert using an unbiased and professional tone. Please limit to 1024 tokens. Do not give any information that is not related to the question, and do not repeat. Say "information is missing on" followed by the related topic, if the given context do not provide sufficient information.
+
+Please cite the contexts with the reference numbers, in the format [citation:x]. If a sentence comes from multiple contexts, please list all applicable citations, like [citation:3][citation:5]. Other than code and specific names and citations, your answer must be written in the same language as the question.
+
+Here are the set of contexts:
+
+{context}
+
+Remember, don't blindly repeat the contexts verbatim. And here is the user question:
+"""
+
+# stop words for removal
+stop_words = [
+    "<|im_end|>",
+    "[End]",
+    "[end]",
+    "\nReferences:\n",
+    "\nSources:\n",
+    "End.",
+]
+
+_more_questions_prompt = """
+You are a helpful assistant that helps the user to ask related questions, based on user's original question and the related contexts. Please identify worthwhile topics that can be follow-ups, and write questions no longer than 20 words each. Please make sure that specifics, like events, names, locations, are included in follow up questions so they can be asked standalone. For example, if the original question asks about "the Manhattan project", in the follow up question, do not just say "the project", but use the full name "the Manhattan project". Your related questions must be in the same language as the original question.
+
+Here are the contexts of the question:
+
+{context}
+
+Remember, based on the original question and related contexts, suggest three such further questions. Do NOT repeat the original question. Each related question should be no longer than 20 words. Here is the original question:
+"""
+
+
+def search_with_serper(query : str, key : str):
+    """
+    Search with Serper API and return the contexts
+    """
+    payload = json.dumps(
+        {
+            "query" : query,
+            "num" : (
+                REFERENCES if REFERENCES % 10 == 0 else (REFERENCES // 10 + 1) * 10
+            ),
+        }
+    )
+
+    headers = {"X-API-KEY": key, "ContentType" : "application/json"}
+    logger.info(
+        f'{payload} {headers} {key} {query} {SERPER_SEARCH_ENDPOINT}'
+    )
+    response = requests.post(
+        SERPER_SEARCH_ENDPOINT,
+        headers = headers,
+        data = payload,
+        timeout = DEFAULT_SEARCH_ENGINE_TIMEOUT
+    )
+    
