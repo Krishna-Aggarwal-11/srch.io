8178d98ce93f512bcbc562ea42aef7685b221d35
Krishna-Aggarwal <krishnaaggarwal1105@gmail.com>
2024-02-18 00:10:56 +0530

    Adding Search Page

--- webclient/frontend/src/pages/Search.jsx
+++ webclient/frontend/src/pages/Search.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import SearchBar from './../components/SearchBar';
+import Title from './../components/Title.jsx';
+import Result from './../components/Result.jsx';
+import { useLocation } from "react-router-dom";
 
 const Search = () => {
+  const location = useLocation();
+  const path = useLocation().pathname;
+  const params = new URLSearchParams(location.search);
+  const q = params.get('q');
+  console.log(q)
   return (
-    <div>Search</div>
+    <div className="absolute inset-0 bg-[url('/ui/bg.svg')]">
+    <div className="mx-auto max-w-3xl absolute inset-4 md:inset-8 bg-white">
+      <div className="h-20 pointer-events-none rounded-t-2xl w-full backdrop-filter absolute top-0 bg-gradient-to-t from-transparent to-white [mask-image:linear-gradient(to_bottom,white,transparent)]"></div>
+      <div className="px-4 md:px-8 pt-6 pb-24 rounded-2xl ring-8 ring-zinc-300/20 border border-zinc-200 h-full overflow-auto">
+        <Title query={q} ></Title>
+        <Result ></Result>
+      </div>
+      <div className="h-80 pointer-events-none w-full rounded-b-2xl backdrop-filter absolute bottom-0 bg-gradient-to-b from-transparent to-white [mask-image:linear-gradient(to_top,white,transparent)]"></div>
+      <div className="absolute z-10 flex items-center justify-center bottom-6 px-4 md:px-8 w-full">
+        <div className="w-full">
+          <SearchBar></SearchBar>
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
 
